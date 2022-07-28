6dc11de84ea7bb17fd6fe943c3f93bcbd160a586
Evernayt <110179244+Evernayt@users.noreply.github.com>
2022-07-28 19:00:29 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace AutoShortcut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbxCurrentFolder = new System.Windows.Forms.TextBox();
+            this.btnCurrentFolder = new System.Windows.Forms.Button();
+            this.btnCreateShortcuts = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxCurrentFolder
+            // 
+            this.tbxCurrentFolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxCurrentFolder.Enabled = false;
+            this.tbxCurrentFolder.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbxCurrentFolder.Location = new System.Drawing.Point(12, 25);
+            this.tbxCurrentFolder.Name = "tbxCurrentFolder";
+            this.tbxCurrentFolder.ReadOnly = true;
+            this.tbxCurrentFolder.Size = new System.Drawing.Size(275, 23);
+            this.tbxCurrentFolder.TabIndex = 0;
+            // 
+            // btnCurrentFolder
+            // 
+            this.btnCurrentFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCurrentFolder.Location = new System.Drawing.Point(293, 25);
+            this.btnCurrentFolder.Name = "btnCurrentFolder";
+            this.btnCurrentFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnCurrentFolder.TabIndex = 1;
+            this.btnCurrentFolder.Text = "Выбрать";
+            this.btnCurrentFolder.UseVisualStyleBackColor = true;
+            this.btnCurrentFolder.Click += new System.EventHandler(this.btnCurrentFolder_Click);
+            // 
+            // btnCreateShortcuts
+            // 
+            this.btnCreateShortcuts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateShortcuts.Location = new System.Drawing.Point(243, 54);
+            this.btnCreateShortcuts.Name = "btnCreateShortcuts";
+            this.btnCreateShortcuts.Size = new System.Drawing.Size(125, 23);
+            this.btnCreateShortcuts.TabIndex = 2;
+            this.btnCreateShortcuts.Text = "Создать ярлыки";
+            this.btnCreateShortcuts.UseVisualStyleBackColor = true;
+            this.btnCreateShortcuts.Click += new System.EventHandler(this.btnCreateShortcuts_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Папка текущего месяца";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(380, 90);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreateShortcuts);
+            this.Controls.Add(this.btnCurrentFolder);
+            this.Controls.Add(this.tbxCurrentFolder);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(396, 128);
+            this.MinimumSize = new System.Drawing.Size(396, 128);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Shortcut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxCurrentFolder;
+        private System.Windows.Forms.Button btnCurrentFolder;
+        private System.Windows.Forms.Button btnCreateShortcuts;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using AutoShortcut.Properties;
+using IWshRuntimeLibrary;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AutoShortcut
+{
+    public partial class Form1 : Form
+    {
+        string currentFolder = Settings.Default.currentFolder;
+        string iconsDir = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\Auto Shortcut Icons";
+        string folderTemp, folderPrintPhoto, folderSublim, folderPhotomontage;
+        string[] iconsArray = { "\\month.ico", "\\temp.ico", "\\photoPrint.ico", "\\sublim.ico", "\\photomontage.ico" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbxCurrentFolder.Text = Settings.Default.currentFolder;
+        }
+
+        /// <summary>
+        /// Создать ярлык Windows
+        /// </summary>
+        /// <param name="TargetPath">Файл, для которого нужно создать ярлык</param>
+        /// <param name="ShortcutFile">Путь и имя файла ярлыка, включая расширение файла (.lnk)</param>
+        /// <param name="Description">Описание ярлыка</param>
+        /// <param name="Arguments">Аргументы командной строки</param>
+        /// <param name="HotKey">Горячая клавиша быстрого доступа в виде строки, например "Ctrl+F"</param>
+        /// <param name="WorkingDirectory">Параметр быстрого доступа "Начать в"</param>
+        public static void CreateShortcut(string TargetPath, string ShortcutFile, string Description,
+           string Arguments, string HotKey, string WorkingDirectory, string IconLocation)
+        {
+            // Сначала проверьте необходимые параметры:
+            if (String.IsNullOrEmpty(TargetPath))
+                throw new ArgumentNullException("TargetPath");
+            if (String.IsNullOrEmpty(ShortcutFile))
+                throw new ArgumentNullException("ShortcutFile");
+
+            // Создайте экземпляр WshShellClass:
+            var wshShell = new WshShellClass();
+
+            // Создать объект ярлыка:
+            IWshShortcut shorcut = (IWshShortcut)wshShell.CreateShortcut(ShortcutFile);
+
+            // Назначьте свойства ярлыка:
+            shorcut.TargetPath = TargetPath;
+            shorcut.Description = Description;
+            if (!String.IsNullOrEmpty(Arguments))
+                shorcut.Arguments = Arguments;
+            if (!String.IsNullOrEmpty(HotKey))
+                shorcut.Hotkey = HotKey;
+            if (!String.IsNullOrEmpty(WorkingDirectory))
+                shorcut.WorkingDirectory = WorkingDirectory;
+
+            shorcut.IconLocation = IconLocation;
+
+            // Сохраните ярлык:
+            shorcut.Save();
+        }
+
+        private bool FolderSearch()
+        {
+            DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(currentFolder);
+            DirectoryInfo[] dirsInDirTemp = hdDirectoryInWhichToSearch.GetDirectories("*ВРЕМЕННО*.*");
+            DirectoryInfo[] dirsInDirPrintPhoto = hdDirectoryInWhichToSearch.GetDirectories("*ПЕЧАТЬ ФОТО*.*");
+            DirectoryInfo[] dirsInDirSublim = hdDirectoryInWhichToSearch.GetDirectories("*СУБЛИМАЦИЯ*.*");
+            DirectoryInfo[] dirsInDirPhotomontage = hdDirectoryInWhichToSearch.GetDirectories("*ФОТОМОНТАЖ*.*");
+
+            foreach (DirectoryInfo foundDir in dirsInDirTemp)
+                folderTemp = foundDir.FullName;
+
+            foreach (DirectoryInfo foundDir in dirsInDirPrintPhoto)
+                folderPrintPhoto = foundDir.FullName;
+
+            foreach (DirectoryInfo foundDir in dirsInDirSublim)
+                folderSublim = foundDir.FullName;
+
+            foreach (DirectoryInfo foundDir in dirsInDirPhotomontage)
+                folderPhotomontage = foundDir.FullName;
+
+            if (folderTemp == null || folderPrintPhoto == null || folderSublim == null || folderPhotomontage == null)
+                return false;
+            else
+                return true;
+        }
+
+        private void btnCurrentFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.SelectedPath = Settings.Default.currentFolder;
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                currentFolder = FBD.SelectedPath;
+                tbxCurrentFolder.Text = currentFolder;
+                Settings.Default.currentFolder = currentFolder;
+                Settings.Default.Save();
+            }
+        }
+
+        private void btnCreateShortcuts_Click(object sender, EventArgs e)
+        {
+            if (tbxCurrentFolder.Text != "")
+            {
+                Directory.CreateDirectory(iconsDir);
+
+                using (FileStream stream = new FileStream(iconsDir + iconsArray[0], FileMode.Create))
+                    Resources.month.Save(stream);
+
+                using (FileStream stream = new FileStream(iconsDir + iconsArray[1], FileMode.Create))
+                    Resources.temp.Save(stream);
+
+                using (FileStream stream = new FileStream(iconsDir + iconsArray[2], FileMode.Create))
+                    Resources.photoPrint.Save(stream);
+
+                using (FileStream stream = new FileStream(iconsDir + iconsArray[3], FileMode.Create))
+                    Resources.sublim.Save(stream);
+
+                using (FileStream stream = new FileStream(iconsDir + iconsArray[4], FileMode.Create))
+                    Resources.photomontage.Save(stream);
+
+                Shortcut_Create();
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Не указана текущая папка месяца.\nНажмите ОК, чтобы указать.", 
+                    Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+                if (result == DialogResult.OK)
+                    btnCurrentFolder.PerformClick();
+            }
+        }
+
+        private void Shortcut_Create()
+        {
+            if (FolderSearch())
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string resultPath = currentFolder.Substring(currentFolder.LastIndexOf(" ")).Replace(" ", "");
+
+                CreateShortcut(currentFolder, desktopPath + "\\" + resultPath + ".lnk", null, null, null, null, iconsDir + iconsArray[0]);
+                CreateShortcut(folderTemp, desktopPath + "\\ВРЕМЕННО.lnk", null, null, null, null, iconsDir + iconsArray[1]);
+                CreateShortcut(folderPrintPhoto, desktopPath + "\\ПЕЧАТЬ ФОТО.lnk", null, null, null, null, iconsDir + iconsArray[2]);
+                CreateShortcut(folderSublim, desktopPath + "\\СУБЛИМАЦИЯ.lnk", null, null, null, null, iconsDir + iconsArray[3]);
+                CreateShortcut(folderPhotomontage, desktopPath + "\\ФОТОМОНТАЖ.lnk", null, null, null, null, iconsDir + iconsArray[4]);
+            }
+            else
+            {
+                MessageBox.Show("Указана неправильная папка!",
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
